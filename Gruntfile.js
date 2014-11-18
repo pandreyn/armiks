39355146d502449dddfdfddbc6951887e08b99e7
pandreyn <pandreyn@gmail.com>
2014-11-18 17:45:08 +0300

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -470,8 +470,8 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('default', [
-        'newer:jshint',
-        'test',
+        //'newer:jshint',
+        //'test',
         'build'
     ]);
 };
